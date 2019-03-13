c2aa2dd8199866873da9f0eb74f1c67d279e4215
javaspace71 <javaspace71@gmail.com>
2019-03-12 17:11:21 -0700

    Lesson 7 - encryption
    Uses Caesar Cipher to encrypt messages.  User must provide key as well.

---
+++ IntrotoPython/s3messages_encryption.py
@@ -0,0 +1,50 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 0
+original_message = ''
+secretalphabet = ''
+newMessage = ''
+
+def prompt_encrypt():
+  print("\n :-) :-) :-) :-0 SUPER SECRET SPY SYSTEM  ??????????\n")
+  print("super secret spy system is watching you now ...")
+  print("you can encrypt your message now")
+  global original_message
+  original_message = input('Please enter a message: ')
+  key = input('Enter a key (1-26): ')
+  key = int(key)
+  print("\n")
+  print(alphabet)
+  return key
+
+def secretalpha(key):
+  global secretalphabet
+  for character in alphabet:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    secretcharacter = alphabet[newPosition]
+    secretalphabet = secretalphabet + secretcharacter
+  print(secretalphabet)
+  print("\n")
+  
+
+def encrypt(message, key):
+  global newMessage
+  for character in message:
+    if character in alphabet:
+      position = alphabet.find(character)
+      newPosition = (position + key) % 26
+      newCharacter = alphabet[newPosition]
+      newMessage = newMessage + newCharacter
+    else:
+      newMessage = newMessage + character
+
+
+#the main program logic starts below
+  
+key = prompt_encrypt()
+secretalpha(key)
+encrypt(original_message, key)
+print('Your new excrypted message is: ', newMessage)
+
+
+
