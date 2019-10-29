c8aab76c5ce8eaa2201d984b0b6ea820657a9b1c
javaspace71 <javaspace71@gmail.com>
2019-10-29 10:34:09 -0700

    Add files via upload

---
+++ IntrotoPython/timeprint.py
@@ -0,0 +1,30 @@
+import datetime
+import calendar
+import time;
+
+print("\n")
+
+localtime = time.asctime( time.localtime(time.time()) )
+print ("Local current time :", localtime)
+
+print("\n")
+
+cal = calendar.month(2019, 2)
+print ("Here is the calendar:")
+print (cal)
+
+print("\n")
+
+"""
+currentDT = datetime.datetime.now()
+
+print ("Current Year is: %d" % currentDT.year)
+print ("Current Month is: %d" % currentDT.month)
+print ("Current Day is: %d" % currentDT.day)
+print ("Current Hour is: %d" % currentDT.hour)
+print ("Current Minute is: %d" % currentDT.minute)
+print ("Current Second is: %d" % currentDT.second)
+print ("Current Microsecond is: %d" % currentDT.microsecond)
+"""
+
+
